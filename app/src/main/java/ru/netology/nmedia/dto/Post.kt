aa38114a7af822aa88cb47bbb8def8eb9de13fc4
sashel007 <sashel007@yandex.ru>
2023-11-25 20:53:28 +0300

    Initial commit

---
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -0,0 +1,12 @@
+package ru.netology.nmedia.dto
+
+data class Post(
+    val id: Long,
+    val author: String,
+    val content: String,
+    val published: Int,
+    val likedByMe: Boolean,
+    val likes: Int = 0
+//    var sharings: Int,
+//    var video: String? = ""
+) 
